3bb1edd11fb02394caa5807ef14a6b7f0e79b5d7
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2015-05-05 09:04:02 +0200

    Unblock exe file, otherwise InstallUtil shows strange errors.

--- install-server.bat
+++ install-server.bat
@@ -4,6 +4,9 @@ set PATH=%PATH%;%windir%\Microsoft.NET\Framework\v4.0.30319
 :: build the radius server
 :: msbuild server/RDSFactor.sln /property:Configuration=release
 
+:: Unblock the downloaded file, otherwise stupid output from InstallUtil
+powershell -Command {Unblock-File -Path server\bin\Release\RDSFactor.exe}
+
 :: install it
 InstallUtil server\bin\Release\RDSFactor.exe
 
